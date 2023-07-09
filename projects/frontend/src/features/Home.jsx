6ba0eac0d95e394c7d5041ad1efd9f196a9e03cd
Angela Busato <busato.angela@gmail.com>
2023-07-09 19:41:01 +0200

    feat: add react router link

--- projects/frontend/src/features/Home.jsx
+++ projects/frontend/src/features/Home.jsx
@@ -1,7 +1,9 @@
+import { Link } from 'react-router-dom'
+
 export const Home = () => {
   return (
-    <div className="text-3xl font-bold underline">
-      <button>Login</button>
+    <div className="grid h-screen place-items-center">
+      <Link className="border border-blue-700 rounded p-2" to="/">Login with Spotify</Link>
     </div>
   )
 }
