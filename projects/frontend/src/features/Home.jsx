fdbdcc7ace8f1df39016e695f747f64c304f9787
Angela Busato <busato.angela@gmail.com>
2023-07-13 19:25:29 +0200

    feat: add login button

--- projects/frontend/src/features/Home.jsx
+++ projects/frontend/src/features/Home.jsx
@@ -1,9 +1,26 @@
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { getHealth } from '../services/health'
+
+const API_URL = import.meta.env.VITE_API_URL
 
 export const Home = () => {
+  const [health, setHealth] = useState()
+
+  useEffect(() => {
+   getHealth().then((response) => setHealth(response))
+  }, [])
+
+  console.log(health)
+  
+  const handleClick = () => {
+    window.location.href = API_URL + '/login/spotify'
+  }
+
   return (
     <div className="grid h-screen place-items-center">
-      <Link className="border border-blue-700 rounded p-2" to="/">Login with Spotify</Link>
+      <button className="border border-blue-700 rounded p-2" onClick={handleClick}>
+        Login with Spotify
+      </button>
     </div>
   )
 }
