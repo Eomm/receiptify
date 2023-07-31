7670cdc358dbf066eb52362a3fc70947e8a9ce9f
Manuel Spigolon <behemoth89@gmail.com>
2023-07-31 08:40:22 +0200

    feat: list layout

--- projects/frontend/src/routes/StatsPage.jsx
+++ projects/frontend/src/routes/StatsPage.jsx
@@ -51,13 +51,42 @@ export const StatsPage = () => {
       {/* Display the fetched data in a list */}
       <div className="container mx-auto mt-16">
         <ul>
-          {statsData.map((item, index) => (
-            <li key={index} className="bg-gray-100 p-4 mb-2 rounded-md">
-              <span className="font-bold">{index+1}</span>
-              <span className="ml-4">{item.name}</span>
-              <span className="ml-4">{item.popularity}</span>
-            </li>
-          ))}
+        <div className="grid grid-cols-3 gap-4">
+        {statsData.map((item, index) => (
+          <div key={item.id} className="bg-white rounded-lg shadow-md p-4 relative">
+            <img
+              src={item.images[0].url}
+              alt={item.name}
+              className="w-full h-40 object-cover rounded-lg"
+            />
+             <div className="absolute top-0 left-0 bg-teal-700 text-white font-semibold rounded-full p-2 m-2 border-2 border-rose-500">
+              {index + 1}
+            </div>
+            <div className="mt-4">
+              <h2 className="text-xl font-semibold">{item.name}</h2>
+              <p className="text-gray-600">Popularity: {item.popularity}</p>
+              <div className="flex flex-wrap mt-2">
+                {item.genres.map((genre) => (
+                  <span
+                    key={genre}
+                    className="px-2 py-1 bg-teal-200 text-gray-800 rounded-full mr-2 mb-2 text-sm"
+                  >
+                    {genre}
+                  </span>
+                ))}
+              </div>
+              <a
+                href={item.external_urls.spotify}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="block mt-2 text-blue-600 hover:underline hover:text-rose-400"
+              >
+                Spotify Profile
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
         </ul>
       </div>
     </>
